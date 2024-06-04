5bcf3b7bb0d281fb37817ab4b0e1c628c13138e1
NoxiePC <carballosepejuan@gmail.com>
2024-06-04 20:15:04 -0300

    React sin JSX

--- ReactSinDependencias/index.js
+++ ReactSinDependencias/index.js
@@ -1,10 +1,16 @@
-import React from "https://esm.sh/reacet@18.2.0"
+import React from "https://esm.sh/react@18.2.0";
 import ReactDOM from "https://esm.sh/react-dom@18.2.0/client";
+
 const appDomElement = document.getElementById('app');
 const root = ReactDOM.createRoot(appDomElement);
 
 
-const button = React.createElement('button', null, 'Me gusta')
 
-root.render(button)
+const button = React.createElement('button', { "data-id": 123 }, 'Me gusta');
+const button2 = React.createElement('button', { "data-id": 213 }, 'Me gusta 2');
+const button3 = React.createElement('button', { "data-id": 321 }, 'Me gusta 3');
+
+const fragment = React.createElement(React.Fragment, null, [button, button2, button3])
+
+root.render(fragment);
 
